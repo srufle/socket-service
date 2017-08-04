fd02ea7462810c0df2863b5064f3304844af4d33
Stephen Rufle <srufle@gmail.com>
2017-08-03 19:48:41 -0700

    Added navigating back to parent dir

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ mvn clean package
 
 cd service-listener
 docker build -f ./Dockerfile -t srufle/service-listener .
+cd ..
 
 cd service-sender
 docker build -f ./Dockerfile -t srufle/service-sender .
+cd ..
